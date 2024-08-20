a37fc20e5b61dea2c43db6bbd1f704e62482df50
João Victor Correa <correa.joaov25@gmail.com>
2024-08-19 22:01:58 -0300

    Rename component 'chart-types-violence-situations' to 'chart-violence-situations-types'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartAssistanceTypesComponent } from './components/chart-assistance-types/chart-assistance-types.component';
-import { ChartViolenceSituationsTypesComponent } from './components/chart-types-violence-situations/chart-violence-situations-types.component';
+import { ChartViolenceSituationsTypesComponent } from './components/chart-violence-situations-types/chart-violence-situations-types.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
