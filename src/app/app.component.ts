113769670e3846f9e024ffd44bca347ad15fffcd
João Victor Correa <correa.joaov25@gmail.com>
2024-10-24 17:01:35 -0300

    Formatting the quantity chart with thousand separators and no decimal places

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
 })
 export class AppComponent {
   title = 'tcc-dashboard';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
